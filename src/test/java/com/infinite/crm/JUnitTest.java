ca08d14c73a757d239413bc078ed5381c7d82af5
nithinkumar2468 <nithinkumarkodipyaka@gmail.com>
2024-03-20 17:11:53 +0530

    commit2

---
+++ src/test/java/com/infinite/crm/JUnitTest.java
@@ -0,0 +1,13 @@
+package com.infinite.crm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JUnitTest {
+	@Test
+	public void multiply() {
+		assertEquals(4,2*2);
+	}
+
+}
