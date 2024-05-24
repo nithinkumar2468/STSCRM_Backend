39862ff2fa84e88d5b3a9336145036df5af522b7
nithinkumar2468 <nithinkumarkodipyaka@gmail.com>
2024-05-24 14:33:25 +0530

    deploy-to-railway

--- src/main/java/com/infinite/crm/CrmApplication.java
+++ src/main/java/com/infinite/crm/CrmApplication.java
@@ -27,43 +27,46 @@ import com.infinite.crm.service.Email;
 @EnableFeignClients("com.infinite.crm")
 //@EnableDiscoveryClient
 public class CrmApplication {
-	
-	
+
 	@Autowired
 	private Email emailService;
-	
+
 	@Autowired
 	private UserRepository userRepo;
-	
+
 	@Autowired
 	private TicketRepository ticketRepo;
-	
+
 	@Autowired
 	private AdminRepository adminRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
 	}
-	
-	@Scheduled(cron = "0 */2 * ? * *")
-    public void emailScheduler() {
-        List<Ticket> tickets = ticketRepo.findAll();
 
-        for (Ticket ticket : tickets) {
-            if (ticket.getStatus().equalsIgnoreCase("done")) {
-                if (ticket.getEmail() != null) {
-                    emailService.sendEmail(ticket.getEmail(), "Your Issue is Resolved..!",
-                            "Your issue has been resolved..!");
-                }
-            } else if (ticket.getStatus().equalsIgnoreCase("active")) {
-                List<Admin> admins = adminRepo.findAll();
-                List<String> adminEmails = admins.stream().map(Admin::getEmail).collect(Collectors.toList());
-                emailService.sendEmailToMultipleRecipients(adminEmails, "Ticket is Pending to resolve",
-                        "Please Resolve the ticket..!");
-            }
-        }
-    }
-	
+	@Scheduled(cron = "0 */2 * ? * *")
+	public void emailScheduler() {
+		List<Ticket> tickets = ticketRepo.findAll();
+
+		for (Ticket ticket : tickets) {
+			if (ticket.getStatus().equalsIgnoreCase("done")) {
+				if (ticket.getEmail() != null) {
+					emailService.sendEmail(ticket.getEmail(), "Your Issue is Resolved..! - " + ticket.getTid(),
+							"Dear " + ticket.getUsername() + "," + '\n'
+									+ "This is to notify you that your issue regarding " + ticket.getIssue()
+									+ " has been successfully Resolved..!"+ '\n'
+									+"Regards,"+ '\n'
+									+"admin-helpdesk");
+				}
+			} else if (ticket.getStatus().equalsIgnoreCase("active")) {
+				List<Admin> admins = adminRepo.findAll();
+				List<String> adminEmails = admins.stream().map(Admin::getEmail).collect(Collectors.toList());
+				emailService.sendEmailToMultipleRecipients(adminEmails, "Ticket is Pending to resolve",
+						"Please Resolve the ticket..!");
+			}
+		}
+	}
+
 	@Bean
 	ModelMapper modelMapper() {
 		return new ModelMapper();
