abc25b0aeaac2443c55230f455b2f995e0934ede
nithinkumar2468 <nithinkumarkodipyaka@gmail.com>
2024-05-28 22:18:08 +0530

    deploy

--- src/main/java/com/infinite/crm/controller/OrdersController.java
+++ src/main/java/com/infinite/crm/controller/OrdersController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.infinite.crm.dto.OrdersDTO;
 import com.infinite.crm.model.Orders;
+import com.infinite.crm.model.User;
+import com.infinite.crm.repository.UserRepository;
 import com.infinite.crm.service.Email;
 import com.infinite.crm.service.OrdersService;
 
@@ -24,32 +26,41 @@ import com.infinite.crm.service.OrdersService;
 @CrossOrigin("https://master.dfhb2sx7j66q1.amplifyapp.com")
 @RequestMapping("api/n1")
 public class OrdersController {
-	
-	private Logger logger=LoggerFactory.getLogger(this.getClass());
-	
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private OrdersService service;
-	
+
 	@Autowired
 	private Email emailService;
-	
-	@PostMapping("/{useremail}/order")
-	Orders newOrders(@PathVariable String useremail,@RequestBody OrdersDTO newOrders) {
-		
-		LocalDateTime myDateObj = LocalDateTime.now();   
-	    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm a");  
-	    
-	    String formattedDate = myDateObj.format(myFormatObj);  
-	    
-	    newOrders.setOrdereddate(formattedDate);
-	    
-	    logger.info("Users Email is :"+useremail);
-	    
-	    emailService.sendEmail(useremail,"Orders Placed Successfuly..!" ,"Thanks for your Order" );
 
-		return service.addOrders(useremail,newOrders);
+	@Autowired
+	private UserRepository repo;
+
+	@PostMapping("/{useremail}/order")
+	Orders newOrders(@PathVariable String useremail, @RequestBody OrdersDTO newOrders) {
+
+		LocalDateTime myDateObj = LocalDateTime.now();
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm a");
+
+		String formattedDate = myDateObj.format(myFormatObj);
+
+		newOrders.setOrdereddate(formattedDate);
+
+		logger.info("Users Email is :" + useremail);
+
+		User userdetails = repo.findByEmail(useremail);
+
+		logger.info(userdetails.getName());
+
+		emailService.sendEmail(useremail, "Your Order Confirmation - Thank You for Your Purchase!", "Hi "
+				+ userdetails.getName() + "," + '\n' + '\n' + "Thank you for your order! " + '\n' + '\n'
+				+ "We’re excited to let you know that we’ve received your order and it’s now being processed. Below are the details of your purchase:");
+
+		return service.addOrders(useremail, newOrders);
 	}
-	
+
 	@GetMapping("/{useremail}/orders")
 	List<OrdersDTO> getAllOrdersbyemail(@PathVariable String useremail) {
 		return service.findAllOrders(useremail);
